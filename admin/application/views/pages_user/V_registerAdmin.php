3e95458428cf4464ec7ac2019f8c7823ce28c506
Richardyogi <richardwilliam02@gmail.com>
2019-04-27 01:09:46 +0700

    [ADD] UI add game, Register game, and Register booster

---
+++ admin/application/views/pages_user/V_registerAdmin.php
@@ -0,0 +1,42 @@
+<div class="container-fluid">
+                        
+                        <!-- start page title -->
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="page-title-box">
+                                    <div class="page-title-right">
+                                        <ol class="breadcrumb m-0">
+                                            <li class="breadcrumb-item"><a href="#">Warboost</a></li>
+                                            <li class="breadcrumb-item active">Register Admin</li>
+                                        </ol>
+                                    </div>
+                                    <h4 class="page-title">Register Admin</h4>
+                                </div>
+                            </div>
+                        </div> 
+            
+                        <form class="form-horizontal" action="">
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Name</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" value="">
+                                    </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Username</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" value="">
+                                    </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Password</label>
+                                <div class="col-sm-10">
+                                    <input type="password" class="form-control" value="password">
+                                </div>
+                            </div>
+
+                        </form>
+                    
+            </div>
---
+++ admin/application/views/pages_user/V_registerBooster.php
@@ -0,0 +1,109 @@
+<div class="container-fluid">
+                        
+                        <!-- start page title -->
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="page-title-box">
+                                    <div class="page-title-right">
+                                        <ol class="breadcrumb m-0">
+                                            <li class="breadcrumb-item"><a href="#">Warboost</a></li>
+                                            <li class="breadcrumb-item active">Register Booster</li>
+                                        </ol>
+                                    </div>
+                                    <h4 class="page-title">Register Booster</h4>
+                                </div>
+                            </div>
+                        </div> 
+            
+                        <form class="form-horizontal" action="">
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Username</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" value="">
+                                    </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Password</label>
+                                <div class="col-sm-10">
+                                    <input type="password" class="form-control" value="password">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Name</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" value="">
+                                    </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Nickname(In Game)</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" value="">
+                                    </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Tanggal Lahir</label>
+                                <div class="col-sm-10">
+                                    <input class="form-control" type="date" name="date">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Handphone</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" value="">
+                                    </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label" for="example-email">Email</label>
+                                <div class="col-sm-10">
+                                    <input type="email" id="example-email" name="example-email" class="form-control" placeholder="Email">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Rating</label>
+                                    <div class="col-sm-10">
+                                        <select class="form-control">
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </select>
+                                    </div>
+                            </div>
+                            
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Status</label>
+                                    <div class="col-sm-10">
+                                        <select class="form-control">
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                        </select>
+                                    </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Profile Picture</label>
+                                    <div class="col-sm-10">
+                                        <input type="file" class="form-control">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">About Me</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" value="">
+                                    </div>
+                            </div>
+
+
+                        </form>
+                    
+            </div>
